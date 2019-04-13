03d5988f0a45fcd6356ea08d8380bfbb416ff8f9
MichaelRomero <liikemike@aim.com>
2019-04-13 17:14:14 -0400

    commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   resources :events
-  resources :users, only: [:create]
+  resources :users
   post '/login', to: 'authentication#create'
   get '/current_user', to: 'authentication#show'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
