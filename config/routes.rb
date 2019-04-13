671c7552d1e3cd60a25186bd83c4a2459a84cbf5
Cristian Leiva <cleiva94.cl@gmail.com>
2019-04-13 16:36:12 -0400

    merge

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   resources :events
   resources :users, only: [:create]
-  post '/login', to: 'auth#create'
-  get '/current_user', to: 'auth#show'
+  post '/login', to: 'authentication#create'
+  get '/current_user', to: 'authentication#show'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
