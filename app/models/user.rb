34b87adf0c73e80c24e7cf7e2d212934f0c48cc5
Cristian Leiva <cleiva94.cl@gmail.com>
2019-04-13 15:16:30 -0400

    backend auth done?

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,8 @@
 class User < ApplicationRecord
   has_many :events
+  has_secure_password
+  validates :username, presence: true
+  validates :username, uniqueness: true
+  validates :email, presence: true
+  validates :email, uniqueness: true
 end
