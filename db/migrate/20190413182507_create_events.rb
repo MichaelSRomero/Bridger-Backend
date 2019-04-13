88be9ee04d35d22837c4619a9600ee912c9f9cde
MichaelRomero <liikemike@aim.com>
2019-04-13 15:00:02 -0400

    fixed migrations for events

--- db/migrate/20190413182507_create_events.rb
+++ db/migrate/20190413182507_create_events.rb
@@ -6,8 +6,6 @@ class CreateEvents < ActiveRecord::Migration[5.2]
       t.integer :sleep
       t.integer :diet
       t.integer :social_activity
-      t.belongs_to :user, foreign_key: true
-
       t.timestamps
     end
   end
